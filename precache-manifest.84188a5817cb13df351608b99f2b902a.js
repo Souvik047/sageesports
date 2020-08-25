bcfd36315e334be5d9255c5111be17871722b7d2
Souvik047 <55787258+Souvik047@users.noreply.github.com>
2020-08-25 20:57:43 +0530

    Updates

---
+++ precache-manifest.84188a5817cb13df351608b99f2b902a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9abd2fffb467ca92f9f86da606c60117",
+    "url": "/sageesports/index.html"
+  },
+  {
+    "revision": "d5fa8b3b99176d4e4140",
+    "url": "/sageesports/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "14b47554d6a412c4792d",
+    "url": "/sageesports/static/css/main.b4389e82.chunk.css"
+  },
+  {
+    "revision": "d5fa8b3b99176d4e4140",
+    "url": "/sageesports/static/js/2.bee1c11b.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/sageesports/static/js/2.bee1c11b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14b47554d6a412c4792d",
+    "url": "/sageesports/static/js/main.f4ffb35c.chunk.js"
+  },
+  {
+    "revision": "0ce12e6b921fddce5000",
+    "url": "/sageesports/static/js/runtime-main.40f80096.js"
+  },
+  {
+    "revision": "7e0984662f268d637c892d24f19b3752",
+    "url": "/sageesports/static/media/Eelric.7e098466.png"
+  },
+  {
+    "revision": "de999f4a9e5dfc124a583596b4ab8b43",
+    "url": "/sageesports/static/media/File_001.de999f4a.png"
+  },
+  {
+    "revision": "81379ea8a38eb6a64874287f7d23b7f7",
+    "url": "/sageesports/static/media/Main.81379ea8.jpg"
+  },
+  {
+    "revision": "3ea30239a92015d4c984a593bea5ad83",
+    "url": "/sageesports/static/media/Shahil.3ea30239.png"
+  },
+  {
+    "revision": "e77ef12f5150f6bb5d9f5df06f7872e9",
+    "url": "/sageesports/static/media/Souvik.e77ef12f.png"
+  }
+]);
